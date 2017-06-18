c15a8f1e127b6030f64d94bb06ea85d3eb739781
Artem <khaapt@gmail.com>
2017-06-18 17:10:48 +0300

    redux store

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -12,35 +12,38 @@ const initialState = [
   {
     name: 'Alena Vuran',
     score: 50,
-  }
+  },
 ];
 
-export default function Player(state = initialState, action){
-  swith (action.type) {
+export default function Player(state=initialState, action) {
+  switch(action.type) {
     case PlayerActionTypes.ADD_PLAYER:
       return [
         ...state,
         {
-          name: acton.name,
+          name: action.name,
           score: 0
         }
       ];
+
     case PlayerActionTypes.REMOVE_PLAYER:
       return [
         ...state.slice(0, action.index),
         ...state.slice(action.index + 1)
       ];
+
     case PlayerActionTypes.UPDATE_PLAYER_SCORE:
-      return state.map ((player, index) => {
-        if (index === action.index) {
+      return state.map((player, index) => {
+        if(index === action.index) {
           return {
-             ...player,
-             score: player.score + action.score
+            ...player,
+            score: player.score + action.score
           };
         }
         return player;
       });
+
     default:
-    retrun state;
+      return state;
   }
 }
